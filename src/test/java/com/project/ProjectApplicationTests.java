338fb5c8cb05e4f663156a090e247ed3d6ddc0df
Qing2514 <2514632453@qq.com>
2023-08-18 13:26:06 +0800

    新增sm4国密算法测试用例

--- src/test/java/com/project/ProjectApplicationTests.java
+++ src/test/java/com/project/ProjectApplicationTests.java
@@ -1,10 +1,29 @@
 package com.project;
 
+import cn.hutool.core.util.CharsetUtil;
+import cn.hutool.crypto.SmUtil;
+import cn.hutool.crypto.symmetric.SymmetricCrypto;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class ProjectApplicationTests {
+
+    @Value("${sm4.key}")
+    private String key;
+
+    @Test
+    public void sm4Encrypt() {
+        String plaintext2 = "123456789";
+        SymmetricCrypto sm42 = SmUtil.sm4(key.getBytes());
+        String ciphertext2 = sm42.encryptHex(plaintext2);
+        String ciphertext3 = sm42.encryptHex(plaintext2);
+        String decryptStr = sm42.decryptStr(ciphertext2, CharsetUtil.CHARSET_UTF_8);
+        System.out.println("encryptHex加密结果: " + ciphertext2);
+        System.out.println("encryptHex加密结果: " + ciphertext3);
+        System.out.println("decryptStr解密结果: " + decryptStr);
+    }
 
     @Test
     public void test() {
