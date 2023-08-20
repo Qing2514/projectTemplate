b3c0686169f831fd2312660b6d0aea3651611806
Qing2514 <2514632453@qq.com>
2023-08-20 16:04:01 +0800

    新增sm2国密算法测试用例

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 ## projectTemplate
 
 ### 介绍
-springboot后端开发模板
+Spring Boot 后端开发模板，用于快速构建后端项目。
 
 ### 相关技术
 技术 | 说明
 --- | ---
+Spring Boot | 后端框架
 Spring Security | 安全框架
 MySQL |数据库
 Druid | 数据库连接池
@@ -14,6 +15,6 @@ Mybatis Plus Generator | 代码生成器
 Lombok | 注解工具
 Redis | 缓存框架
 MinIO | 文件存储
-Swagger UI | 接口文档
+Knife4j | 接口文档
 Docker | 容器
 Json | 前后端交互
--- src/test/java/com/project/ProjectApplicationTests.java
+++ src/test/java/com/project/ProjectApplicationTests.java
@@ -1,8 +1,14 @@
 package com.project;
 
 import cn.hutool.core.util.CharsetUtil;
+import cn.hutool.core.util.HexUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.crypto.BCUtil;
 import cn.hutool.crypto.SmUtil;
+import cn.hutool.crypto.asymmetric.KeyType;
+import cn.hutool.crypto.asymmetric.SM2;
 import cn.hutool.crypto.symmetric.SymmetricCrypto;
+import org.bouncycastle.jcajce.provider.asymmetric.ec.BCECPublicKey;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,23 +16,71 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class ProjectApplicationTests {
 
+    @Value("${sm2.privateKey}")
+    private String privateKey;
+
+    @Value("${sm2.publicKey}")
+    private String publicKey;
+
     @Value("${sm4.key}")
     private String key;
 
-    @Test
-    public void sm4Encrypt() {
-        String plaintext2 = "123456789";
-        SymmetricCrypto sm42 = SmUtil.sm4(key.getBytes());
-        String ciphertext2 = sm42.encryptHex(plaintext2);
-        String ciphertext3 = sm42.encryptHex(plaintext2);
-        String decryptStr = sm42.decryptStr(ciphertext2, CharsetUtil.CHARSET_UTF_8);
-        System.out.println("encryptHex加密结果: " + ciphertext2);
-        System.out.println("encryptHex加密结果: " + ciphertext3);
-        System.out.println("decryptStr解密结果: " + decryptStr);
-    }
+    String text = "123456789123456789";
 
     @Test
     public void test() {
+    }
+
+    @Test
+    public void sm2GenerateKey() {
+        SM2 sm2 = SmUtil.sm2();
+        // 生成私钥
+        byte[] privateKey = BCUtil.encodeECPrivateKey(sm2.getPrivateKey());
+        String privateKeyString = HexUtil.encodeHexStr(privateKey);
+        System.out.println("私钥: " + privateKeyString);
+        System.out.println("私钥位数：" + privateKeyString.length());
+        // 生成66位公钥
+        byte[] publicKeyEc = BCUtil.encodeECPublicKey(sm2.getPublicKey());
+        String publicKeyEcString = HexUtil.encodeHexStr(publicKeyEc);
+        System.out.println("公钥: " + publicKeyEcString);
+        System.out.println("公钥位数：" + publicKeyEcString.length());
+        // 生成130位公钥
+        byte[] publicKey = ((BCECPublicKey)sm2.getPublicKey()).getQ().getEncoded(false);
+        String publicKeyString = HexUtil.encodeHexStr(publicKey);
+        System.out.println("未压缩公钥: " + publicKeyString);
+        System.out.println("未压缩公钥位数：" + publicKeyString.length());
+    }
+
+    @Test
+    public void sm2Encrypt() {
+        SM2 sm2 = SmUtil.sm2(privateKey, publicKey);
+        System.out.println("私钥：" + privateKey);
+        System.out.println("私钥位数：" + privateKey.length());
+        System.out.println("公钥：" + publicKey);
+        System.out.println("公钥位数：" + publicKey.length());
+        // 公钥加密
+        String encryptStr = sm2.encryptBcd(text, KeyType.PublicKey);
+        System.out.println("公钥加密：" + encryptStr);
+        System.out.println("加密后位数：" + encryptStr.length());
+        // 私钥解密
+        String decryptStr = StrUtil.utf8Str(sm2.decryptFromBcd(encryptStr, KeyType.PrivateKey));
+        System.out.println("私钥解密：" + decryptStr);
+        System.out.println("解密后位数：" + decryptStr.length());
+    }
+
+    @Test
+    public void sm4Encrypt() {
+        SymmetricCrypto sm4 = SmUtil.sm4(key.getBytes());
+        System.out.println("秘钥：" + key);
+        System.out.println("秘钥位数：" + key.length());
+        // 加密
+        String ciphertext = sm4.encryptHex(text);
+        System.out.println("加密结果: " + ciphertext);
+        System.out.println("加密后位数：" + ciphertext.length());
+        // 解密
+        String decryptStr = sm4.decryptStr(ciphertext, CharsetUtil.CHARSET_UTF_8);
+        System.out.println("解密结果: " + decryptStr);
+        System.out.println("解密后位数：" + decryptStr.length());
     }
 
 }
